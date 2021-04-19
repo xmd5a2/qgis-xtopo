587e1c24db5a09677c2f3087f98a4edea23c6244
xmd5a <lornaderek@mail.ru>
2021-04-19 20:03:26 +0300

    Update style

--- build_gui.sh
+++ build_gui.sh
@@ -5,8 +5,8 @@ cp -f process_bbox.py gui/
 cd gui
 pyinstaller --onefile -i /src/images/logo_icon.ico --noconsole --clean qgis-xtopo-gui-linux.spec
 docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pyinstaller --onefile -i /src/images/logo_icon.ico --noconsole --clean qgis-xtopo-gui-win.spec"
-upx -9 -o dist/qgis-xtopo-gui-upx.exe dist/qgis-xtopo-gui.exe
-rm -f dist/qgis-xtopo-gui.exe
-mv dist/qgis-xtopo-gui-upx.exe dist/qgis-xtopo-gui.exe
+# upx -9 -o dist/qgis-xtopo-gui-upx.exe dist/qgis-xtopo-gui.exe
+# rm -f dist/qgis-xtopo-gui.exe
+# mv dist/qgis-xtopo-gui-upx.exe dist/qgis-xtopo-gui.exe
 rm -f calc_srtm_tiles_list.py
 rm -f process_bbox.py
